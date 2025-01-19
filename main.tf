7c152253b96109b40bd8e0944a5a00329036921d
Didier Gourillon <dgourillon@google.com>
2025-01-19 22:41:17 +0100

    split provder.tf in separate file

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-provider "google" {
-  alias			        = "s3nscloud"
-  project			    = "s3ns:dgourillon-test"
-  region		    	= "u-france-east1"
-  universe_domain		= "s3nsapis.fr"
-}
+
 
 resource "google_compute_network" "main" {
   provider                = google.s3nscloud
---
+++ provider.tf
@@ -0,0 +1,8 @@
+
+
+provider "google" {
+  alias			        = "s3nscloud"
+  project			    = "s3ns:dgourillon-test"
+  region		    	= "u-france-east1"
+  universe_domain		= "s3nsapis.fr"
+}
